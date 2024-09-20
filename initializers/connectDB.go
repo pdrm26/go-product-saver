d6ba4764089d5b997aebb310dca6d2dee06a0f52
pedram <peeeedii@gmail.com>
2024-09-20 19:26:18 +0330

    feat: add gorm to handle db

---
+++ initializers/connectDB.go
@@ -0,0 +1,28 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(config *Config) {
+	var err error
+	dsn := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		config.DBHost, config.DBPort, config.DBUserName, config.DBUserPassword, config.DBName)
+
+	DB, err = gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to connect to the Database")
+	}
+	fmt.Println("🚀 Connected Successfully to the Database")
+}
